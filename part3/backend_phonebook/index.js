f1a5bb7b6dff4f372f27fb39d60ac2be74b0a637
Vishnudath M <vishnuda333@gmail.com>
2025-11-25 03:29:14 +0530

    part3-deploying-ex 3.9-3.11
    trial1

--- part3/backend_phonebook/index.js
+++ part3/backend_phonebook/index.js
@@ -4,10 +4,12 @@ const morgan=require("morgan")
 const cors=require("cors")
 app.use(express.json())
 app.use(cors())
+app.use(express.static("dist"))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body `))
 
-const PORT=3001
+
+const PORT=process.env.PORT||3001
 
 
 const generateId=()=>{
